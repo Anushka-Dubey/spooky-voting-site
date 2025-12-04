e3e8f9cb0f86cc206c119d3fd6cbbe62bddecbb3
Anushka Dubey <98007835+Anushka-Dubey@users.noreply.github.com>
2025-12-04 18:06:26 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// *** THIS MUST POINT TO './App.js' (assuming you renamed the main file in src) ***
+// *** NEW: Importing the global CSS file ***
+import './index.css'; 
+
+// This must point to the file you just renamed: App.js
 import App from './App.js'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
